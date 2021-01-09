4294c51b2f59a026eb91258662ce89a685afee2c
Christian-Alexander-BB <S10205535@connect.np.edu.sg>
2021-01-09 15:39:32 +0800

    Test Receival of Spotify Access Token

--- scripts/script.js
+++ scripts/script.js
@@ -25,4 +25,37 @@ $(document).ready(function () {
 			}
 		});
 	});
+
+		// $.ajax({
+	// 	type: 'GET',
+	// 	url: 'https://api.spotify.com/v1/search?q=tania%20bowra&type=artist',
+	// 	contentType: "application/json",
+	// 	headers: {
+	// 		"Accept": "application/json",
+	// 		"content-type": "application/json",
+	// 		"Authorization": "Bearer    BQCWRewmJQJFNJohKwd-X46pldWI551uTdD3C9RKnp3iRsuzNVnYBZJGTTvSwUuvHY6wwruLxp9hCziARaYT0WPoTvVH4xhiozALIl7JJUC_iK8CFo3HDnYHQAH vkWkY8O4rNT0ROYziB_gN0PM8O6cmcol2Lk"
+	// 	}
+	// })
+
+var clientID = "6642631f98474b689bf4baf30817a702"
+var clientSecret = "2ec5414d075b427eadeb0282b66e2c19"
+var accessToken = ""
+
+	$.ajax({
+		method: 'POST',
+		url: "https://accounts.spotify.com/api/token",
+		timeout: 0,
+		data: {
+			grant_type: "client_credentials"
+		},
+		headers: {
+			"Content-Type": "application/x-www-form-urlencoded",
+			Authorization: "Basic " + btoa(clientID + ":" + clientSecret)
+		},
+		success: function(data) {
+			console.log(data);
+			accessToken = data.access_token;
+			console.log(accessToken)
+		}
+	});
 });
