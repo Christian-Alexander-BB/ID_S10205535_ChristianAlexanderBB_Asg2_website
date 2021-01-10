180c75144a0c1168f2a7f687c083760152e13268
Christian-Alexander-BB <S10205535@connect.np.edu.sg>
2021-01-10 20:45:55 +0800

    Initial Webpage Design w/ Bootstrap

--- scripts/script.js
+++ scripts/script.js
@@ -71,7 +71,7 @@ function findSimilarArtist(data, token) {
 
 // The bottom function obtains details about the song, artist, album, or any other music detail based on returned spotify 
 // search results
-function findDetails(artistTag) {
+function findDetails(artistTag, artistSpotify) {
 	let artist = $(artistTag).text();
 
 	$.ajax({
@@ -84,7 +84,7 @@ function findDetails(artistTag) {
 
 			else {
 				console.log(`Artist ${$(artistTag).text()} found in database.`);
-				artistDetailsToScreen(data, artistTag);
+				artistDetailsToScreen(data, artistTag, artistSpotify);
 			}
 		},
 		error: function() {
@@ -99,13 +99,13 @@ function similarArtistToScreen(data) {
 	let artistVals = []
 	var artistOrder = []
 
-	$('#test').append("<b>Recommended Artists<b><br>");
+	$('#test').append("<h1 class='text-center display-6 text-muted fw-lighter colors-1' id='arrow'>V Scroll Down V</h1><h2 class='text-center display-4 colors-1' id='recommended-artists'>Recommended Artists</h2><br>");
 
 	x = 1
 	for (i = 0; i < data.tracks.length; i++) {
 
 		if (allArtists.includes(data.tracks[i].artists[0].name) == false) {
-			$('#test').append(`<p id=artist-${i}>${data.tracks[i].artists[0].name}</p><br>`);
+			$('#test').append(`<div class="text-center" id="artist-${i}"><button class="colors-2 button-no-borders fw-light fs-5">${data.tracks[i].artists[0].name}</button><br><br></div>`);
 			artistVals.push(`#artist-${x}`);
 			artistOrder.push(i);
 			allArtists.push(data.tracks[i].artists[0].name);
@@ -119,16 +119,16 @@ function similarArtistToScreen(data) {
 		console.log("hello " + allArtists[0])
 		let targetID = e.target.id
 		let id = `#${targetID}`
-		// let artistSpotify = data.tracks[artistOrder[id.indexOf()]].artists[0].external-urls.spotify
 		let artistSpotify = data.tracks[0].artists[0].external_urls.spotify
 		console.log(artistSpotify)
 		
-		findDetails(id);
+		findDetails(id, artistSpotify);
 	})
 }
 
-function artistDetailsToScreen(data, artistTag) {
+function artistDetailsToScreen(data, artistTag, artistSpotify) {
 	$('.description').empty()
+	console.log("artist spotify: " + artistSpotify);
 	allArtistFacts = data.artists[0]
 	artistFacts = [
 		allArtistFacts.strArtistAlternate,
@@ -144,13 +144,14 @@ function artistDetailsToScreen(data, artistTag) {
 	]
 
 	for (i = 0; i < artistFacts.length; i++) {
-		if (artistFacts[i] == null) {
-			artistFacts[i] = "Null"
+		if (artistFacts[i] == "") {
+			artistFacts[i] = "null"
 		}
 	}
 
 	$(artistTag).append(
 		`
+		<div class="fw-light fs-6 d-grid gap-2 col-8 mx-auto" id="details">
 		<p class="description">Alternate Name: ${artistFacts[0]}</p>
 		<p class="description">Genre: ${artistFacts[1]}</p>
 		<p class="description">Style: ${artistFacts[2]}</p>
@@ -161,7 +162,9 @@ function artistDetailsToScreen(data, artistTag) {
 		<p class="description">Country of Origin: ${artistFacts[7]}</p>
 		<p class="description">Gender: ${artistFacts[8]}</p>
 		<p class="description">Biography: ${artistFacts[9]}</p>
-		<button class="description" id="link-to-spotify">Artist's Spotify Profile</button>
+		<form action='${artistSpotify}' method="get" class="d-grid gap-2 col-2 mx-auto" target="_blank">
+		<button type="submit" class="btn btn-outline-secondary" id="artist-spotify">Artist's Spotify Profile</button>
+		</div>
 		`
 	)
 }
